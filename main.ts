088603ab22b684ead54cb644578a23d43e874947
aarnavvj <81868372+aarnavvj@users.noreply.github.com>
2021-07-21 20:08:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,41 +5,34 @@ input.onButtonPressed(Button.B, function () {
     // This game was based on the old classic game " Flappy Bird. "
     Bird.change(LedSpriteProperty.Y, 1)
 })
+let Empty_Obstacle_Y = 0
 let Ticks = 0
 let Bird: game.LedSprite = null
+game.setLife(3)
+let Index = 0
+let Obstacles: game.LedSprite[] = []
 Bird = game.createSprite(0, 2)
 Bird.set(LedSpriteProperty.Blink, 300)
-let Obstacles: game.LedSprite[] = []
-let Empty_Obstacle_Y = randint(0, 4)
-for (let index = 0; index <= 4; index++) {
-    if (index != Empty_Obstacle_Y) {
-        Obstacles.push(game.createSprite(4, index))
-    }
-}
 basic.forever(function () {
-    for (let Obstacle of Obstacles) {
-        Obstacle.change(LedSpriteProperty.X, -1)
-    }
-    basic.pause(500)
     while (Obstacles.length > 0 && Obstacles[0].get(LedSpriteProperty.X) == 0) {
         Obstacles.removeAt(0).delete()
     }
     for (let Obstacle of Obstacles) {
         Obstacle.change(LedSpriteProperty.X, -1)
     }
-    if (Ticks % 3 == 0) {
+    if (Ticks % 5 == 0) {
         Empty_Obstacle_Y = randint(0, 4)
-        for (let index = 0; index <= 4; index++) {
-            if (index != Empty_Obstacle_Y) {
-                Obstacles.push(game.createSprite(4, index))
+        for (let Index_2 = 0; Index_2 <= 4; Index_2++) {
+            if (Index_2 != Empty_Obstacle_Y) {
+                Obstacles.push(game.createSprite(4, Index_2))
             }
         }
     }
-    for (let Obstacle of Obstacles) {
-        if (Obstacle.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && Obstacle.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
-            game.gameOver()
+    for (let Obstacle_2 of Obstacles) {
+        if (Obstacle_2.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && Obstacle_2.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
+            game.removeLife(1)
         }
     }
     Ticks += 1
-    basic.pause(500)
+    basic.pause(435)
 })
